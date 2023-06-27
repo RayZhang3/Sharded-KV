274a9be1a9ac5d4c04fbeadd3ddf3df37b56d5fd
Reeve-Zhang <wipe_data@163.com>
2023-06-27 10:10:51 -0700

    Pass Lab4A first test with asynchronously fetching config

--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -25,6 +25,46 @@ type Op struct {
 	Optype   string
 }
 
+const (
+	KVOperation     = 1
+	ConfigOperation = 2
+)
+
+type CommandMsg struct {
+	CommandType int
+	Data        interface{}
+}
+
+func (commandMsg *CommandMsg) String() string {
+	switch commandMsg.CommandType {
+	case KVOperation:
+		return fmt.Sprintf("CommandMsg: CommandType: %s, Data: %v", "KVOperation", commandMsg.Data)
+	case ConfigOperation:
+		return fmt.Sprintf("CommandMsg: CommandType: %s, Data: %v", "ConfigOperation", commandMsg.Data)
+	}
+	return fmt.Sprintf("CommandMsg: CommandType: %s, Data: %v", "Unknown", commandMsg.Data)
+}
+
+/*
+// A configuration -- an assignment of shards to groups.
+// Please don't change this.
+type Config struct {
+	Num    int              // config number
+	Shards [NShards]int     // shard -> gid
+	Groups map[int][]string // gid -> servers[]
+}
+*/
+
+type ConfigMsg struct {
+	Num    int                      // config number
+	Shards [shardctrler.NShards]int // shard -> gid
+	Groups map[int][]string         // gid -> servers[]
+}
+
+func (config *ConfigMsg) String() string {
+	return fmt.Sprintf("ConfigMsg: Num: %v, Shards: %v, Groups: %v", config.Num, config.Shards, config.Groups)
+}
+
 func (op *Op) String() string {
 	switch op.Optype {
 	case "Get":
@@ -51,7 +91,7 @@ type ShardKV struct {
 
 	// Your definitions here.
 	currentState      map[string]string
-	waitChan          map[int]chan Op
+	waitChan          map[int]chan CommandMsg
 	clientSeq         map[int64]int
 	lastIncludedIndex int // for snapshot
 
@@ -59,19 +99,93 @@ type ShardKV struct {
 	mck        *shardctrler.Clerk
 	config     shardctrler.Config
 	prevConfig shardctrler.Config
+	chanConfig chan Op // receive by getLastConfig(), If apply current config(with all shards serves), send prev config num to chanConfig
 }
+
+// 1.Check if there's new config. If not, sleep for 100ms
+// 2.If there's new config,
+// 3.Put the config into raft, Wait for the new config to be applied (Commit by raft)
+// 4.If the new config is applied, set the shard state. If the new config is not applied, go to step 3
+// 5.Set the shards state and wait
 
 func (kv *ShardKV) getLastConfig() {
 	for !kv.killed() {
-		newConfig := kv.mck.Query(-1)
-		if newConfig.Num > kv.config.Num {
-			kv.mu.Lock()
-			kv.prevConfig = kv.config
-			kv.config = newConfig
-			PrettyDebug(dServer, "KVServer %d-%d get new config %v\n", kv.gid, kv.me, kv.config.String())
-			kv.mu.Unlock()
+		_, isLeader := kv.rf.GetState()
+		if !isLeader {
+			time.Sleep(100 * time.Millisecond)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+		newConfig := kv.mck.Query(-1)
+		kv.mu.Lock()
+		needToUpdate := kv.config.Num < newConfig.Num
+		PrettyDebug(dServer, "KVServer %d-%d needToUpdate: %v, kv.config.Num: %d, newConfig.Num: %d\n", kv.gid, kv.me, needToUpdate, kv.config.Num, newConfig.Num)
+		kv.mu.Unlock()
+		if !needToUpdate || !isLeader {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		// Need to update, put the config into raft
+		// Wait for the new config to be applied (Commit by raft)
+		configCommmand := ConfigMsg{Num: newConfig.Num, Shards: newConfig.Shards, Groups: newConfig.Groups}
+		msg := CommandMsg{ConfigOperation, configCommmand}
+
+		// Loop until the new config is committed by raft, and set the shard state
+		for !kv.killed() {
+			index, _, isLeader := kv.rf.Start(msg)
+			PrettyDebug(dServer, "KVServer %d-%d insert new config to raft %s\n", kv.gid, kv.me, configCommmand.String())
+			// If not leader, break the loop
+			if !isLeader {
+				break
+			}
+			kv.mu.Lock()
+			if kv.config.Num >= newConfig.Num {
+				kv.mu.Unlock()
+				break
+			}
+			waitChan := kv.getWaitCh(index)
+			kv.mu.Unlock()
+			// wait for reply
+			select {
+			case <-time.After(1e9): //7e8
+				// Time out, Append the configCommand to log again.
+				PrettyDebug(dServer, "KVServer %d-%d insert config and %s time out\n", kv.gid, kv.me, configCommmand.String())
+				go func() {
+					kv.mu.Lock()
+					close(waitChan)
+					delete(kv.waitChan, index)
+					kv.mu.Unlock()
+				}()
+				continue
+			case commandMsg := <-waitChan:
+				go func() {
+					kv.mu.Lock()
+					close(waitChan)
+					delete(kv.waitChan, index)
+					kv.mu.Unlock()
+				}()
+				if commandMsg.CommandType != ConfigOperation {
+					break
+				}
+				PrettyDebug(dInfo, "KVServer %d-%d receive config ApplyHandler %s\n", kv.gid, kv.me, commandMsg.String())
+				appliedConfig := commandMsg.Data.(ConfigMsg)
+				if appliedConfig.Num < newConfig.Num {
+					continue
+				}
+				if newConfig.Num == 0 || appliedConfig.Num == newConfig.Num {
+					kv.mu.Lock()
+					/*
+						kv.prevConfig = kv.config
+						kv.config = newConfig
+					*/
+					// need to update the state of the shards.
+					PrettyDebug(dServer, "KVServer %d-%d receive new config from raft %s\n", kv.gid, kv.me, kv.config.String())
+					kv.mu.Unlock()
+					break
+				}
+
+			} // end of select
+			// TODO: wait for the shards to change their state.
+		} // The shards state is updated.
 	}
 }
 
@@ -118,6 +232,7 @@ func (kv *ShardKV) String() {
 
 func (kv *ShardKV) applyHandler() {
 	for !kv.killed() {
+		var msg CommandMsg
 		select {
 		case applyMsg := <-kv.applyCh:
 			if applyMsg.SnapshotValid == true {
@@ -128,56 +243,84 @@ func (kv *ShardKV) applyHandler() {
 				}
 				kv.readServerPersistData(applyMsg.Snapshot)
 				kv.lastIncludedIndex = applyMsg.SnapshotIndex
-				PrettyDebug(dPersist, "Server %d receive snapshot with applyMsg.SnapshotIndex %d, kv.currentState:%v, kv.clientSeq:%v", kv.me, applyMsg.SnapshotIndex, kv.currentState, kv.clientSeq)
+				PrettyDebug(dPersist, "KVServer %d-%d receive snapshot with applyMsg.SnapshotIndex %d, kv.currentState:%v, kv.clientSeq:%v",
+					kv.gid, kv.me, applyMsg.SnapshotIndex, kv.currentState, kv.clientSeq)
 				kv.mu.Unlock()
 			} else {
-				op, _ := applyMsg.Command.(Op)
-				PrettyDebug(dServer, "Server%d get valid command %s", kv.me, op.String())
-				appliedOp := Op{op.ClientID, op.SeqNum, op.Key, op.Value, op.Optype}
+				commandMsg := applyMsg.Command.(CommandMsg)
 				kv.mu.Lock()
-				// Check if the applyMsg have been applied before
-				if applyMsg.CommandIndex <= kv.lastIncludedIndex {
-					kv.mu.Unlock()
-					continue
-				}
-				// Check if the client session is existing, if not, init one with 0
-				_, clientIsPresent := kv.clientSeq[appliedOp.ClientID]
-				if !clientIsPresent {
-					kv.clientSeq[appliedOp.ClientID] = 0
-				}
-				// Check duplicated command, If sequenceNum already processed from client, reply OK with stored response
-				if kv.clientSeq[appliedOp.ClientID] >= appliedOp.SeqNum {
+				switch commandMsg.CommandType {
+				case KVOperation:
+					op, _ := commandMsg.Data.(Op)
+					PrettyDebug(dServer, "KVServer %d-%d APPLY HANDLER get Op command %s", kv.gid, kv.me, op.String())
+					appliedOp := Op{op.ClientID, op.SeqNum, op.Key, op.Value, op.Optype}
+					// Check if the applyMsg have been applied before
+					if applyMsg.CommandIndex <= kv.lastIncludedIndex {
+						kv.mu.Unlock()
+						continue
+					}
+					// Check if the client session is existing, if not, init one with 0
+					_, clientIsPresent := kv.clientSeq[appliedOp.ClientID]
+					if !clientIsPresent {
+						kv.clientSeq[appliedOp.ClientID] = 0
+					}
+					// Check duplicated command, If sequenceNum already processed from client, reply OK with stored response
 					if appliedOp.Optype == "Get" {
 						appliedOp.Value = kv.currentState[appliedOp.Key]
 					}
-					PrettyDebug(dServer, "Server%d sequenceNum already processed from client %d, appliedOp: %s, kv.clientSeq: %v",
-						kv.me, appliedOp.ClientID, appliedOp.String(), kv.clientSeq)
-				} else {
-					// If sequenceNum not processed, store response and reply OK
-					if appliedOp.Optype == "Get" {
-						appliedOp.Value = kv.currentState[appliedOp.Key]
+					if kv.clientSeq[appliedOp.ClientID] >= appliedOp.SeqNum {
+						PrettyDebug(dServer, "KVServer %d-%d sequenceNum already processed from client %d, appliedOp: %s, kv.clientSeq: %v",
+							kv.gid, kv.me, appliedOp.ClientID, appliedOp.String(), kv.clientSeq)
+					} else {
+						// If sequenceNum not processed, store response and reply OK
+						kv.applyToStateMachine(&appliedOp)
+						PrettyDebug(dServer, "KVServer %d-%d apply command to STATEMACHINE %s, kv.currentState:%v",
+							kv.gid, kv.me, appliedOp.String(), kv.currentState)
 					}
-					kv.applyToStateMachine(&appliedOp)
-					PrettyDebug(dServer, "Server%d apply command %s, kv.currentState:%v", kv.me, appliedOp.String(), kv.currentState)
+					msg = CommandMsg{CommandType: KVOperation, Data: appliedOp}
+
+				case ConfigOperation:
+					applyConfig := commandMsg.Data.(ConfigMsg)
+					PrettyDebug(dServer, "KVServer %d-%d APPLY HANDLER get Config %s", kv.gid, kv.me, applyConfig.String())
+					// Check if the applyMsg have been applied before
+					if applyMsg.CommandIndex <= kv.lastIncludedIndex {
+						kv.mu.Unlock()
+						continue
+					}
+					if kv.config.Num == 0 || applyConfig.Num == kv.config.Num+1 {
+						PrettyDebug(dServer, "KVServer %d-%d UPDATE CONFIG handler get Config %s, kv.config:%v, kv.prevConfig:%v", kv.gid, kv.me, applyConfig.String(), kv.config, kv.prevConfig)
+						kv.prevConfig = kv.config
+						kv.config = shardctrler.Config{applyConfig.Num, applyConfig.Shards, applyConfig.Groups}
+						// update the state of shards.
+					}
+					msg = applyMsg.Command.(CommandMsg)
+
+				default:
+					panic("Unknown command type")
 				}
+
+				// take snapshot and send msg
 
 				if kv.maxraftstate != -1 && kv.rf.GetStateSize() > kv.maxraftstate {
 					snapshotData := kv.getServerPersistData()
 					kv.rf.Snapshot(applyMsg.CommandIndex, snapshotData)
-					PrettyDebug(dPersist, "Server %d store snapshot with commandIndex %d, kv.currentState:%v, kv.clientSeq:%v", kv.me, applyMsg.CommandIndex, kv.currentState, kv.clientSeq)
+					PrettyDebug(dSnap, "Server %d store snapshot with commandIndex %d, kv.currentState:%v, kv.clientSeq:%v", kv.me, applyMsg.CommandIndex, kv.currentState, kv.clientSeq)
 				}
 
 				// If the channel is existing, and the leader is still alive, send the appliedOp to the channel
 				commandIndex := applyMsg.CommandIndex
 				waitChan, chanExisting := kv.waitChan[commandIndex]
+				kv.mu.Unlock()
 				if chanExisting {
 					select {
-					case waitChan <- appliedOp:
+					case waitChan <- msg:
 					case <-time.After(1 * time.Second):
 						fmt.Println("Leader chan timeout")
 					}
+				} else {
+					PrettyDebug(dError, "KVServer %d-%d want to send msg %s , but no channel for commandIndex %d", kv.gid, kv.me, msg.String(), commandIndex)
 				}
-				kv.mu.Unlock()
+
 			}
 		}
 	}
@@ -198,12 +341,12 @@ func (kv *ShardKV) applyToStateMachine(appliedOp *Op) {
 
 // get the wait channel for the index, if not exist, create one
 // channel is used to send the appliedOp to the client
-func (kv *ShardKV) getWaitCh(index int) (waitCh chan Op) {
+func (kv *ShardKV) getWaitCh(index int) (waitCh chan CommandMsg) {
 	waitCh, isPresent := kv.waitChan[index]
 	if isPresent {
 		return waitCh
 	} else {
-		kv.waitChan[index] = make(chan Op, 1)
+		kv.waitChan[index] = make(chan CommandMsg, 1)
 		return kv.waitChan[index]
 	}
 }
@@ -223,8 +366,9 @@ func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	}
 	// Append command to log, replicate and commit it
 	op := Op{args.ClientID, args.SeqNum, args.Key, "", "Get"}
+	commandMsg := CommandMsg{KVOperation, op}
 	PrettyDebug(dServer, "Server%d insert GET command to raft Log, COMMAND %s", kv.me, op.String())
-	index, _, isLeader := kv.rf.Start(op)
+	index, _, isLeader := kv.rf.Start(commandMsg)
 	if !isLeader {
 		reply.Err = ErrWrongLeader
 		return
@@ -242,32 +386,28 @@ func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	case <-time.After(7e8):
 		reply.Err = ErrWrongLeader
 
-	case applyOp, ok := <-waitCh:
-		if !ok {
-			reply.Err = ErrWrongLeader
+	case commandMsg, ok := <-waitCh:
+		defer func() {
 			go func() {
 				kv.mu.Lock()
 				close(waitCh)
 				delete(kv.waitChan, index)
 				kv.mu.Unlock()
 			}()
+		}()
+		if !ok || commandMsg.CommandType != KVOperation {
+			reply.Err = ErrWrongLeader
 			return
 		}
-
-		PrettyDebug(dServer, "Server%d receive GET command from raft, COMMAND %s", kv.me, op.String())
+		applyOp := commandMsg.Data.(Op)
 		if applyOp.SeqNum == args.SeqNum && applyOp.ClientID == args.ClientID {
 			reply.Value = applyOp.Value
 			reply.Err = OK
+			PrettyDebug(dServer, "Server%d receive GET command from raft, COMMAND %s", kv.me, op.String())
 		} else {
 			reply.Err = ErrWrongLeader
 		}
 	}
-	go func() {
-		kv.mu.Lock()
-		close(waitCh)
-		delete(kv.waitChan, index)
-		kv.mu.Unlock()
-	}()
 }
 
 func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
@@ -288,7 +428,8 @@ func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 
 	// Append command to log, replicate and commit it
 	op := Op{args.ClientID, args.SeqNum, args.Key, args.Value, args.Op}
-	index, _, isLeader := kv.rf.Start(op)
+	commandMsg := CommandMsg{KVOperation, op}
+	index, _, isLeader := kv.rf.Start(commandMsg)
 
 	if !isLeader {
 		reply.Err = ErrWrongLeader
@@ -300,7 +441,7 @@ func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 		return
 	}
 
-	PrettyDebug(dServer, "Server%d insert PUTAPPEND command to raft, COMMAND %s", kv.me, op.String())
+	PrettyDebug(dServer, "KVServer %d-%d insert PUTAPPEND command to raft at Index %d, COMMAND %s", kv.gid, kv.me, index, op.String())
 
 	kv.mu.Lock()
 	waitCh := kv.getWaitCh(index)
@@ -315,33 +456,29 @@ func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	select {
 	case <-time.After(7e8):
 		reply.Err = ErrWrongLeader
-	case applyOp, ok := <-waitCh:
-		if !ok {
-			reply.Err = ErrWrongLeader
+	case commandMsg := <-waitCh:
+		PrettyDebug(dServer, "KVServer%d-%d receive PUTAPPEND command from raft, COMMAND %s", kv.gid, kv.me, commandMsg.String())
+
+		defer func() {
 			go func() {
 				kv.mu.Lock()
 				close(waitCh)
 				delete(kv.waitChan, index)
 				kv.mu.Unlock()
 			}()
+		}()
+		if commandMsg.CommandType != KVOperation {
+			reply.Err = ErrWrongLeader
 			return
 		}
-
-		PrettyDebug(dServer, "Server%d receive GET command from raft, COMMAND %s", kv.me, op.String())
-
+		applyOp := commandMsg.Data.(Op)
 		if applyOp.SeqNum == args.SeqNum && applyOp.ClientID == args.ClientID {
 			reply.Err = OK
+			PrettyDebug(dServer, "KVServer%d-%d reply PUTAPPEND command to client, COMMAND %s", kv.me, op.String())
 		} else {
 			reply.Err = ErrWrongLeader
 		}
 	}
-	go func() {
-		kv.mu.Lock()
-		close(waitCh)
-		delete(kv.waitChan, index)
-		kv.mu.Unlock()
-	}()
-
 }
 
 //
@@ -393,6 +530,8 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 	// call labgob.Register on structures you want
 	// Go's RPC library to marshall/unmarshall.
 	labgob.Register(Op{})
+	labgob.Register(CommandMsg{})
+	labgob.Register(ConfigMsg{})
 
 	kv := new(ShardKV)
 	kv.me = me
@@ -404,7 +543,7 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 
 	// Your initialization code here.
 	kv.currentState = make(map[string]string, 0)
-	kv.waitChan = make(map[int]chan Op, 0)
+	kv.waitChan = make(map[int]chan CommandMsg, 0)
 	kv.clientSeq = make(map[int64]int, 0)
 
 	// Use something like this to talk to the shardctrler:
@@ -421,6 +560,7 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 	}
 	kv.config = shardctrler.Config{}
 	kv.prevConfig = shardctrler.Config{}
+
 	go kv.getLastConfig()
 	go kv.applyHandler()
 	return kv
